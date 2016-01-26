9970da77f078855bec4cb444a9e9f613c395487c
orloffv <orloff.v@gmail.com>
2016-01-26 15:32:22 +0300

    Disable waves for special button

--- src/scripts/staffimUtils.waves.js
+++ src/scripts/staffimUtils.waves.js
@@ -5,19 +5,17 @@
             return {
                 restrict: 'C',
                 link: function(scope, element) {
-                    if (element.hasClass('btn-icon') || element.hasClass('btn-float')) {
-                        Waves.attach(element, ['waves-circle']);
-                    }
+                    if (!element.data('no-waves')) {
+                        if (element.hasClass('btn-icon') || element.hasClass('btn-float')) {
+                            Waves.attach(element, ['waves-circle']);
+                        } else if (element.hasClass('btn-light')) {
+                            Waves.attach(element, ['waves-light']);
+                        } else {
+                            Waves.attach(element);
+                        }
 
-                    else if (element.hasClass('btn-light')) {
-                        Waves.attach(element, ['waves-light']);
+                        Waves.init();
                     }
-
-                    else {
-                        Waves.attach(element);
-                    }
-
-                    Waves.init();
                 }
             };
         });
