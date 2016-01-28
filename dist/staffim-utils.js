ce92f400b148f29e161bbe60b5577d7c5c8a3941
orloffv <orloff.v@gmail.com>
2016-01-28 16:56:58 +0300

    Add params for fileManager

--- dist/staffim-utils.js
+++ dist/staffim-utils.js
@@ -794,8 +794,17 @@
     SUFileManager.$inject = ['CONFIG'];
     function SUFileManager(CONFIG) {
         return {
-            remote: function(id) {
-                return CONFIG.apiUrl + '/images/' + id;
+            remote: function(id, params) {
+                var queryString = '';
+                if (!_.isUndefined(params)) {
+                    params = _.extend({
+                        type: 'thumbnail'
+                    }, params);
+
+                    queryString = '?' + $.param({q: JSON.stringify(params)});
+                }
+
+                return CONFIG.apiUrl + '/images/' + id + queryString;
             },
             local: function(path) {
                 return CONFIG.assetsUrl + path;
