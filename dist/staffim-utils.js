77d7f97058a69f679e9a76dba2784002d3e2e697
orloffv <orloff.v@gmail.com>
2016-02-10 11:12:06 +0300

    add underscore getById

--- dist/staffim-utils.js
+++ dist/staffim-utils.js
@@ -291,6 +291,14 @@
 'use strict';
 (function() {
     _.mixin(s.exports());
+
+    _.mixin({
+        getById: function(collection, id) {
+            return _.find(collection, function(model) {
+                return model.id === id;
+            });
+        }
+    })
 })();
 
 'use strict';
--- src/scripts/staffimUtils.underscore.js
+++ src/scripts/staffimUtils.underscore.js
@@ -1,4 +1,12 @@
 'use strict';
 (function() {
     _.mixin(s.exports());
+
+    _.mixin({
+        getById: function(collection, id) {
+            return _.find(collection, function(model) {
+                return model.id === id;
+            });
+        }
+    })
 })();
