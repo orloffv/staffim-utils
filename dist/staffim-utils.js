48e50166a01b54aca7c7a66d36fc0b11a7a2e6f8
orloffv <orloff.v@gmail.com>
2016-02-10 18:54:09 +0300

    Add underscore.removeById

--- dist/staffim-utils.js
+++ dist/staffim-utils.js
@@ -297,8 +297,19 @@
             return _.find(collection, function(model) {
                 return model.id === id;
             });
+        },
+        removeById: function(collection, id) {
+            var index = _.findIndex(collection, function(model) {
+                return _.has(model, 'id') && model.id === id;
+            });
+
+            if (index !== -1) {
+                collection.splice(index, 1);
+            }
+
+            return collection;
         }
-    })
+    });
 })();
 
 'use strict';
--- src/scripts/staffimUtils.underscore.js
+++ src/scripts/staffimUtils.underscore.js
@@ -7,6 +7,17 @@
             return _.find(collection, function(model) {
                 return model.id === id;
             });
+        },
+        removeById: function(collection, id) {
+            var index = _.findIndex(collection, function(model) {
+                return _.has(model, 'id') && model.id === id;
+            });
+
+            if (index !== -1) {
+                collection.splice(index, 1);
+            }
+
+            return collection;
         }
-    })
+    });
 })();
