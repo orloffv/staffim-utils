9834c3e8f695c8bbe28e6c681ac9b0a01fb40aab
orloffv <orloff.v@gmail.com>
2016-04-11 12:12:15 +0300

    Add errorResponse notify

--- src/scripts/staffimUtils.notify.js
+++ src/scripts/staffimUtils.notify.js
@@ -4,11 +4,12 @@
         .run(notifyConfig)
         .service('SUNotify', SUNotify);
 
-    SUNotify.$inject = ['ngNotify'];
-    function SUNotify(ngNotify) {
+    SUNotify.$inject = ['ngNotify', '$injector'];
+    function SUNotify(ngNotify, $injector) {
         var service = {};
         service.success = success;
         service.error = error;
+        service.errorResponse = errorResponse;
         service.info = info;
 
         function success(message, options) {
@@ -21,6 +22,16 @@
 
         function info(message, options) {
             return ngNotify.set(message, _.extend({type: 'info'}, options));
+        }
+
+        function errorResponse(response, message) {
+            var SRErrorTranslator = $injector.get('SRErrorTranslator');
+
+            var translator = new SRErrorTranslator(response.modelName);
+            var errors = translator.parseResponse(response.$response);
+            if (_.size(errors) || message) {
+                this.error(_.size(errors) ? _.toSentence(errors, '<br>', '<br>') : message);
+            }
         }
 
         return service;
